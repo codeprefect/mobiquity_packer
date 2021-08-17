bee6d9ff2dbe9d5c46ff91d91f717b209a3f9c00
Mubarak Imam <mubarakadeimam@gmail.com>
2021-08-17 15:29:40 +0100

    MOB-001: added failing test

--- src/core/packer.test.ts
+++ src/core/packer.test.ts
@@ -1,10 +1,31 @@
-
+import fs from 'fs/promises';
+import path from 'path';
 import { Packer } from './packer';
 
 describe('Packer works!', () => {
     test('pack should return input filePath', () => {
+        // arrange
         var filePath = "hello.txt";
+
+        // act
         var result = Packer.pack(filePath);
+
+        // assert
         expect(result).toEqual(result);
+    });
+
+    test('pack should return expected result', async () => {
+        // arrange
+        const filePath = 'resources/example_input';
+
+        // act
+        var result = Packer.pack(path.join(__dirname, filePath));
+
+        // assert
+        var expected = await fs.readFile(filePath, {
+            encoding: 'utf-8'
+        });
+
+        expect(result).toEqual(expected);
     });
 });
